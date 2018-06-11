143c328bbcc41b6526d972237bf329102a0f0290
Alex <alexanderchapman@outlook.com>
2018-06-11 21:03:50 +0100

    feat: added 2 new test suites
    added test suites for Initial Entries and New Feed Selection.
    Each will contain one test.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -59,18 +59,25 @@ $(function() {
         });
     });
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
+        it('loadFeed function calls and completes',function(){
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
+        it('new feed loaded and content changes',function(){
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        });
+    });
+
 }());
