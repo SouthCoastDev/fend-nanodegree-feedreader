62b67a930b0e2cd24c0263f7163bff5f40c30db4
Alex <alexanderchapman@outlook.com>
2018-06-12 21:04:03 +0100

    feat: added test logic for menu suite
    Added test to check menu hidden by default. Test currently
    passes.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,15 +32,12 @@ $(function() {
         });
     });
 
-
     /* TODO: Write a new test suite named "The menu" */
     describe('The menu', function() {
+
         it('menu hidden by default',function(){
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        var body = document.getElementsByTagName('body')[0];
+         expect(body.classList).toContain('menu-hidden');
         });
 
         it('menu changed when clicked',function(){
